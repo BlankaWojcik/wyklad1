40fa8a738fa87c24f63f09f2a218892ebb70e12f
BlankaWojcik <blanka1705@wp.pl>
2022-01-20 22:05:41 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CFooter from './components/CFooter';
 
 function App() {
   return (
     <div className="App">
+      <CFooter/>
     </div>
   );
 }
