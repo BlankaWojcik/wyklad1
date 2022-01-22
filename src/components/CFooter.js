245a78bbd264ce3fddb33d7e0fb15eadc8926241
BlankaWojcik <blanka1705@wp.pl>
2022-01-23 00:17:21 +0100

    master

--- src/components/CFooter.js
+++ src/components/CFooter.js
@@ -14,16 +14,21 @@ import React from 'react'
 
 
 
+//export const CFooter = () => {
+//    let date = new Date();
+//    return (
+//        <div>
+//            <h5>
+//                PPFront-end, przykładowy serwis React. Dzisiaj mamy:{" "}
+//                {date.toDateString()}{" "}
+//            </h5>
+//        </div>
+//    );
+//}
+
 export const CFooter = () => {
     let date = new Date();
-    return (
-        <div>
-            <h5>
-                PPFront-end, przykładowy serwis React. Dzisiaj mamy:{" "}
-                {date.toDateString()}{" "}
-            </h5>
-        </div>
-    );
+    return React.createElement('div', null, 'PPFront-end, przykładowy serwis React. Dzisiaj mamy:{date.toDateString()}');
 }
 
 //export default CFooter;
