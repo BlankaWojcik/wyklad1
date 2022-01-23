2afdcf4fc4bed148bf2e1713695f5b2b2620259c
BlankaWojcik <blanka1705@wp.pl>
2022-01-23 03:19:48 +0100

    master

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import {CFooter} from './components/CFooter'
 import CHeader from './components/CHeader'
+import CContents from './components/CContents'
 
 function App() {
   return (
     <div className="App">
       <CHeader aName="Jan" aFamily="Kowalski"/>
       {/* <CHeader aName="Tomasz" aFamily="Nowak"/> */}
-
+      <CContents/>
       <CFooter weather="słoneczna" temp="25">
         <p>Niebo jest bezchmurne, wiatr słaby.</p>
       </CFooter>
