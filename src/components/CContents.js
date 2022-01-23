218b3d8dc1584b3165931ab984289411269f3bd4
BlankaWojcik <blanka1705@wp.pl>
2022-01-23 05:04:10 +0100

    master

--- src/components/CContents.js
+++ src/components/CContents.js
@@ -1,14 +1,30 @@
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 import React, {Component} from 'react';
+import './CContents.css'
+import jsonData from '../res/text.json'
+import jsonData2 from '../res/text2.json'
+
 class CContents extends Component{
 
     constructor(){
         super()
         this.state= {msg: 'Witaj użytkowniku. Kliknij przycisk, aby poznać wiadomość'}
+        this.txtData = () => JSON.parse(JSON.stringify(jsonData));
+        this.txtData2 = () => JSON.parse(JSON.stringify(jsonData2));
+        const imgContext = require.context('../res/', false, /\.png$/);
+        let img = {};
+        this.imgs = imgContext.keys().reduce( (icons, file) => {
+            const cname = imgContext(file);
+            const label = file.slice(2, -4);
+            img[label]= cname;
+            return img;
+        }, {});
+        //this.changeMsg = this.changeMsg.bind(this);
     }
 
-    changeMsg(){
-        this.setState({msg: 'Właśnie jesteś uczestnikiem kursu React, gratulacje!'});
+    changeMsg = () =>{
+       this.setState({msg: 'Właśnie jesteś uczestnikiem kursu React, gratulacje!'});
+       console.log('Przycisk został naciśnięty');
     }
 
 
@@ -29,7 +45,7 @@ render(){
             <div className='contentsDivClass'>
                 {/*{items}*/}
                 <h1>{this.state.msg}</h1>
-                <button onClick={()=>this.changeMsg()}>Kliknij, aby poznać tajemnicę</button>
+                <button onClick={this.changeMsg}>Kliknij, aby poznać tajemnicę</button>
             </div>
         )
     }
