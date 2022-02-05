d8afd9a10e74bd7ba3a4eaa9777cf0b557cf4b91
BlankaWojcik <blanka1705@wp.pl>
2022-02-06 00:57:49 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import {CFooter} from './components/CFooter'
-import CHeader from './components/CHeader'
-import CContents from './components/CContents'
-import COuter from './components/COuter'
+//import {CFooter} from './components/CFooter'
+//import CHeader from './components/CHeader'
+//import CContents from './components/CContents'
+//import COuter from './components/COuter'
 
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <CHeader aName="Blanka" aFamily="Wójcik"/>
-      {/* <CHeader aName="Tomasz" aFamily="Nowak"/> */}
-      {/*<CContents/>*/}
-      <COuter/>
-      <CContents/>
-      <CFooter weather="słoneczna" temp="25">
-        <p>Niebo jest bezchmurne, wiatr słaby.</p>
-      </CFooter>
-      {/*<CFooter weather="deszczowa" temp="13"/>
-      <CFooter weather="śnieżna" temp="-6"/> */}
-    </div>
-  );
+import { Component } from "react";
+import HeaderComponent from "./components/HeaderComponent";
+import Navbar from "./components/NavbarComponent";
+
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Rasy from "./pages/Rasy";
+import Home from "./pages/Home";
+import Opieka from "./pages/Opieka";
+import Stadniny from "./pages/Stadniny";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Rasy" element={<Rasy />} />
+            <Route exact path="/Stadniny" element={<Stadniny />} />
+            <Route path="/Opieka" element={<Opieka />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
