ac1844acf657b4dbf37a06f11448a77095065422
BlankaWojcik <blanka1705@wp.pl>
2022-02-06 22:17:29 +0100

    master

--- src/pages/Stadniny.js
+++ src/pages/Stadniny.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React/*, { Component }*/ from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import Navbar from "../components/NavbarComponent";
 import StadninyC from "../components/StadninyComponent";
 
-class Stadniny extends Component {
+/* class Stadniny extends Component {
   render() {
     return (
       <div>
@@ -13,6 +13,18 @@ class Stadniny extends Component {
       </div>
     );
   }
+}
+
+export default Stadniny; */
+
+function Stadniny() {
+  return (
+    <div>
+      <HeaderComponent />
+      <Navbar />
+      <StadninyC />
+    </div>
+  );
 }
 
 export default Stadniny;
