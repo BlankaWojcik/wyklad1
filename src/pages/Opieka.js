0bfdb4038903d7d70de82df606d95b67315fa9ae
BlankaWojcik <blanka1705@wp.pl>
2022-02-06 01:11:53 +0100

    master

---
+++ src/pages/Opieka.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderComponent from "../components/HeaderComponent";
+import Navbar from "../components/NavbarComponent";
+import OpiekaC from "../components/OpiekaComponent";
+
+function Opieka() {
+  return (
+    <div>
+      <HeaderComponent />
+      <Navbar />
+      <OpiekaC />
+    </div>
+  );
+}
+
+export default Opieka;
---
+++ src/pages/Stadniny.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import HeaderComponent from "../components/HeaderComponent";
+import Navbar from "../components/NavbarComponent";
+import StadninyC from "../components/StadninyComponent";
+
+class Stadniny extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderComponent />
+        <Navbar />
+        <StadninyC />
+      </div>
+    );
+  }
+}
+
+export default Stadniny;
